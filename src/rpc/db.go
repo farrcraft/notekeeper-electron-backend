09848e08a3110845bfd2ca77dc4c9d50a0d880eb
sigsegv <sigsegv@quantumfish.com>
2017-03-16 23:09:35 -0700

    handle opening db even if already open.

--- src/rpc/db.go
+++ src/rpc/db.go
@@ -19,17 +19,14 @@ const (
 
 // OpenMasterDb opens the master database in the requested directory
 func (rpc *Server) OpenMasterDb(ctx context.Context, request *pb.OpenMasterDbRequest) (*pb.StatusResponse, error) {
+	// need to close any existing db
+	if rpc.DB != nil {
+		rpc.DB.Close()
+		rpc.DB = nil
+	}
+	rpc.DataPath = filepath.Clean(request.Path)
 	// This is the master index db
 	// There are additional databases where actual notebook data is stored (one DB file per account)
-	appPath := filepath.Clean(request.Path)
-	// Close the old DB if necessary
-	if rpc.DataPath != appPath {
-		if rpc.DB != nil {
-			rpc.DB.Close()
-			rpc.DB = nil
-		}
-		rpc.DataPath = appPath
-	}
 	fileName := filepath.Join(rpc.DataPath, MasterDbFile)
 	rpc.Logger.Info("Opening master db file [", fileName, "]")
 	var err error

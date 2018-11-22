52d617273aea8f718817b036c2b6c36022700ae1
Josh Farr <sigsegv@quantumfish.com>
2018-11-22 13:10:36 -0800

    add more testing.

--- src/account/account_test.go
+++ src/account/account_test.go
@@ -1,22 +1,80 @@
 package account
 
 import (
+	"flag"
 	"os"
 	"testing"
 
 	"../db"
 	"../user"
 
+	"github.com/sirupsen/logrus"
 	"github.com/sirupsen/logrus/hooks/test"
 )
 
-func TestAccount(t *testing.T) {
-	// Setup
-	logger, hook := test.NewNullLogger()
-	registry := db.NewRegistry(logger)
+var harness struct {
+	logger   *logrus.Logger
+	registry *db.Registry
+	hook     *test.Hook
+}
 
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	exitCode := m.Run()
+
+	os.Exit(exitCode)
+}
+
+func setup() {
+	harness.logger, harness.hook = test.NewNullLogger()
+
+	// null logger usually discards all log output, but for debugging we want to send all log output to stdout
+	harness.logger.SetOutput(os.Stdout)
+	harness.logger.SetLevel(logrus.DebugLevel)
+
+	harness.registry = db.NewRegistry(harness.logger)
+}
+
+func teardown(t *testing.T) {
+	// Teardown
+	err := harness.registry.Master.DB.Close()
+	if err != nil {
+		t.Error("Failed to close master db - ", err)
+	}
+	err = os.Remove(harness.registry.Master.Info.Filename)
+	if err != nil {
+		t.Error("Failed to cleanup master db - ", err)
+	}
+
+	for _, handle := range harness.registry.Handles {
+		if handle.Info.Type != db.TypeMaster {
+			err = handle.Close()
+			if err != nil {
+				t.Error("Failed to close db - ", err)
+			}
+			err = os.Remove(handle.Info.Filename)
+			if err != nil {
+				t.Error("Failed to cleanup db - ", err)
+			}
+		}
+	}
+
+	harness.hook.Reset()
+}
+
+func TestAccount(t *testing.T) {
+	setup()
+
+	testCreateAccount(t)
+	testSigninAccount(t)
+
+	teardown(t)
+}
+
+func testCreateAccount(t *testing.T) {
 	accountName := "bob's account"
-	newAccount, err := New(registry, logger, accountName)
+	newAccount, err := New(harness.registry, harness.logger, accountName)
 	if err != nil {
 		t.Error("Expected to create new account - ", err)
 	}
@@ -24,12 +82,12 @@ func TestAccount(t *testing.T) {
 		t.Error("Expected account name to be bob's account")
 	}
 
-	err = registry.OpenMaster("./")
+	err = harness.registry.OpenMaster("./")
 	if err != nil {
 		t.Error("Failed to open master db - ", err)
 	}
 
-	accountIndex := NewIndex(registry, logger)
+	accountIndex := NewIndex(harness.registry, harness.logger)
 
 	count := accountIndex.Count()
 	if count != 0 {
@@ -40,14 +98,14 @@ func TestAccount(t *testing.T) {
 		ID:   newAccount.ID,
 		Type: db.TypeAccount,
 	}
-	accountDBHandle, err := registry.NewHandle(accountDBKey)
+	accountDBHandle, err := harness.registry.NewHandle(accountDBKey)
 	if err != nil {
 		t.Error("Expected to open account db - ", err)
 	}
 
 	userEmail := "bob@notekeeper.io"
 	userPassphrase := "supersecret"
-	newUser, err := user.New(registry, logger, newAccount.ID, userEmail)
+	newUser, err := user.New(harness.registry, harness.logger, newAccount.ID, userEmail)
 	if err != nil {
 		t.Error("Expected to create new user - ", err)
 	}
@@ -68,12 +126,14 @@ func TestAccount(t *testing.T) {
 		ID:   newUser.ID,
 		Type: db.TypeUser,
 	}
-	userDBHandle, err := registry.NewHandle(userDBKey)
+	userDBHandle, err := harness.registry.NewHandle(userDBKey)
 	if err != nil {
 		t.Error("Expected to create user db handle - ", err)
 	}
 
-	// [FIXME] - save user mapping in user index in account db
+	// save user mapping in user index in account db
+	userIndex := user.NewIndex(newAccount.ID, harness.registry, harness.logger)
+	userIndex.Save(newUser, newUser.PassphraseKey)
 
 	// save user
 	err = newUser.Save()
@@ -104,29 +164,75 @@ func TestAccount(t *testing.T) {
 	if count != 1 {
 		t.Error("Expected account index count to be 1")
 	}
+}
 
-	// Teardown
-	err = registry.Master.DB.Close()
+func testSigninAccount(t *testing.T) {
+	// attempt to find the account (lookup)
+	accountName := "bob's account"
+	newAccount, err := New(harness.registry, harness.logger, accountName)
 	if err != nil {
-		t.Error("Failed to close master db - ", err)
-	}
-	err = os.Remove(registry.Master.Info.Filename)
-	if err != nil {
-		t.Error("Failed to cleanup master db - ", err)
+		t.Error("Expected to make new account object - ", err)
 	}
 
-	for _, handle := range registry.Handles {
-		if handle.Info.Type != db.TypeMaster {
-			err = handle.Close()
-			if err != nil {
-				t.Error("Failed to close db - ", err)
-			}
-			err = os.Remove(handle.Info.Filename)
-			if err != nil {
-				t.Error("Failed to cleanup db - ", err)
-			}
+	accountIndex := NewIndex(harness.registry, harness.logger)
+	err = accountIndex.Lookup(newAccount)
+	if err != nil {
+		t.Error("Expected to lookup account in index - ", err)
+	}
+	/*
+		accountDBKey := db.Key{
+			ID:   newAccount.ID,
+			Type: db.TypeAccount,
 		}
+		accountDBHandle, err := harness.registry.GetHandle(accountDBKey)
+		if err != nil {
+			t.Error("Expected to open account db - ", err)
+		}
+	*/
+	// authenticate the user
+	userEmail := "bob@notekeeper.io"
+	userPassphrase := "supersecret"
+	newUser, err := user.New(harness.registry, harness.logger, newAccount.ID, userEmail)
+	if err != nil {
+		t.Error("Expected to create new user - ", err)
 	}
 
-	hook.Reset()
+	// resolve the user id from the user map in the account db
+	userIndex := user.NewIndex(newAccount.ID, harness.registry, harness.logger)
+	err = userIndex.Lookup(newUser)
+	if err != nil {
+		t.Error("Expected to lookup user in index - ", err)
+	}
+	/*
+		// load the user from the user db
+		userDBKey := db.Key{
+			ID:   newUser.ID,
+			Type: db.TypeUser,
+		}
+		_, err = harness.registry.GetHandle(userDBKey)
+		if err != nil {
+			t.Error("Expected to create user db handle - ", err)
+		}
+	*/
+	err = newUser.Load(userPassphrase)
+	if err != nil {
+		t.Error("Expected to load user - ", err)
+	}
+
+	// connect the user to the account & make it the active user
+	newAccount.ActiveUser = newUser
+
+	// set remaining encryption keys
+	newAccount.EncryptedKey = newUser.AccountKey
+	//accountDBHandle.EncryptedKey = newUser.AccountKey
+
+	// load the account
+	err = newAccount.Load()
+	if err != nil {
+		t.Error("Expected to load account - ", err)
+	}
+
+	if newAccount.Name != accountName {
+		t.Error("Expected account name to match")
+	}
 }
--- src/user/user_test.go
+++ src/user/user_test.go
@@ -1,9 +1,128 @@
 package user
 
 import (
+	"flag"
+	"os"
 	"testing"
+
+	"../db"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
 )
 
+var harness struct {
+	logger   *logrus.Logger
+	registry *db.Registry
+	hook     *test.Hook
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	exitCode := m.Run()
+
+	os.Exit(exitCode)
+}
+
+func setup() {
+	harness.logger, harness.hook = test.NewNullLogger()
+
+	// null logger usually discards all log output, but for debugging we want to send all log output to stdout
+	harness.logger.SetOutput(os.Stdout)
+	harness.logger.SetLevel(logrus.DebugLevel)
+
+	harness.registry = db.NewRegistry(harness.logger)
+}
+
+func teardown(t *testing.T) {
+	// Teardown
+	err := harness.registry.Master.DB.Close()
+	if err != nil {
+		t.Error("Failed to close master db - ", err)
+	}
+	err = os.Remove(harness.registry.Master.Info.Filename)
+	if err != nil {
+		t.Error("Failed to cleanup master db - ", err)
+	}
+
+	for _, handle := range harness.registry.Handles {
+		if handle.Info.Type != db.TypeMaster {
+			err = handle.Close()
+			if err != nil {
+				t.Error("Failed to close db - ", err)
+			}
+			err = os.Remove(handle.Info.Filename)
+			if err != nil {
+				t.Error("Failed to cleanup db - ", err)
+			}
+		}
+	}
+
+	harness.hook.Reset()
+}
 func TestUser(t *testing.T) {
+	setup()
+
+	err := harness.registry.OpenMaster("./")
+	if err != nil {
+		t.Error("Failed to open master db - ", err)
+	}
+
+	testSaveUser(t)
+	testLoadUser(t)
+
+	testSaveIndex(t)
+	testLookupIndex(t)
+
+	teardown(t)
+}
+
+func testSaveUser(t *testing.T) {
+	email := "bob@notekeeper.io"
+	userPassphrase := "password"
+	accountID, err := uuid.NewV4()
+	if err != nil {
+		t.Error("Expected to create an account id - ", err)
+	}
+
+	newUser, err := New(harness.registry, harness.logger, accountID, email)
+	if err != nil {
+		t.Error("Expected to create new user - ", err)
+	}
+
+	// create a new db file for the user
+	userDBKey := db.Key{
+		ID:   newUser.ID,
+		Type: db.TypeUser,
+	}
+	userDBHandle, err := harness.registry.NewHandle(userDBKey)
+	if err != nil {
+		t.Error("Expected to create user db - ", err)
+	}
+
+	err = newUser.CreateKeys([]byte(userPassphrase))
+	if err != nil {
+		t.Error("Expected to create user keys - ", err)
+	}
+
+	userDBHandle.EncryptedKey = newUser.UserKey
+
+	err = newUser.Save()
+	if err != nil {
+		t.Error("Expected to save new user - ", err)
+	}
+}
+
+func testLoadUser(t *testing.T) {
+
+}
+
+func testSaveIndex(t *testing.T) {
+
+}
+
+func testLookupIndex(t *testing.T) {
 
 }
